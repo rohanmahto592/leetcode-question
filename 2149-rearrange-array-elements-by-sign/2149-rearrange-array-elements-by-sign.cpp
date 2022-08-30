0399f91fe8d0804d2e310f1c4c3be7acfb2ac571
Rohan Mahto <68339841+rohanmahto592@users.noreply.github.com>
2022-08-30 23:38:12 +0530

    Time: 347 ms (51.44%), Space: 130 MB (48.83%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -5,26 +5,24 @@ public:
         int i=0;
         int j=0;
         int n=nums.size();
-        bool flag1,flag2;
-        while(i<n or j<n)
+        while(i<n and j<n)
         {
-            flag1=flag2=false;
+           
             while(i<n and nums[i]<0)
             {
                 i++;
-                flag1=true;
+                
             }
             while(j<n and nums[j]>0)   
             {
                 j++;
-                flag2=true;
+               
             }
-            finals.push_back(nums[i++]);
-            finals.push_back(nums[j++]);
-            if(i==n or j==n)
-            {
-                break;
-            }
+            finals.push_back(nums[i]);
+            finals.push_back(nums[j]);
+            i++;
+            j++;
+            
           
             
         }
