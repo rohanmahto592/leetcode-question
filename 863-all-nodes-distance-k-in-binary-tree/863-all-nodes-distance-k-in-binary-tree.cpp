546b5e1eae9986a51dc187225485942f98be5918
Rohan Mahto <68339841+rohanmahto592@users.noreply.github.com>
2022-09-25 23:37:57 +0530

    Time: 4 ms (89.72%), Space: 12.3 MB (93.40%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool findpath(TreeNode*root,vector<TreeNode*>&v,int target)
+    {
+        if(root==nullptr)
+        {
+            return false;
+        }
+        v.push_back(root);
+        if(target==root->val)
+        {
+            
+            return true;
+        }
+        
+        if(findpath(root->left,v,target))
+        {
+            return true;
+        }
+        if(findpath(root->right,v,target))
+        {
+            return true;
+        }
+        v.pop_back();
+        
+        return false;
+    
+        
+    }
+    void findnodes(TreeNode*root,int k,TreeNode*check,vector<int>&finals)
+    {
+        if(root==nullptr or root==check)
+        {
+            return;
+        }
+        if(k==0)
+        {
+            finals.push_back(root->val);
+            return;
+        }
+        
+        findnodes(root->left,k-1,check,finals);
+         findnodes(root->right,k-1,check,finals);
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<TreeNode*>v;
+        bool a=findpath(root,v,target->val);
+        for(auto z:v)
+        {
+            cout<<z->val<<" ";
+        }
+        vector<int>finals;
+         int b=k;
+        for(int i=v.size()-1;i>=0;i--)
+        {
+            TreeNode*z=v[i];
+           if(b<0)
+           {
+               break;
+           }
+            findnodes(z,b,b==k?nullptr:v[i+1],finals);  
+            --b;
+            
+        }
+        return finals;
+    }
+};
