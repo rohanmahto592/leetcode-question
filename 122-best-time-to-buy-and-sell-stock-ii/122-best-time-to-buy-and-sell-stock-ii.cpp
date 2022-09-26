c3dc6aa9b1167ba0618bf5b8f35e8e1d04483382
Rohan Mahto <68339841+rohanmahto592@users.noreply.github.com>
2022-09-26 21:43:48 +0530

    Time: 36 ms (5.01%), Space: 17.3 MB (10.45%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,30 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        
-        int mini=prices[0];
-        int maxi=0;
-        int currentmax=0,totalsum=0;
-        for(int i=1;i<prices.size();i++)
+class Solution
+{
+    public:
+        int sellstock(int idx, vector<int> &prices,int flag,vector<vector<int>>&dp)
         {
-            mini=min(mini,prices[i]);
-            maxi=max(prices[i],mini);
-            if(maxi-mini>currentmax)
+            if (idx == prices.size())
             {
-                currentmax=maxi-mini;
+                return 0;
+            }
+            if(dp[idx][flag]!=-1)
+            {
+                return dp[idx][flag];
+            }
+            
+            if (flag==1)
+            {
+               return dp[idx][flag]= max(-prices[idx] + sellstock(idx + 1, prices, 0,dp), sellstock(idx + 1, prices, 1,dp));
             }
             else
             {
-                totalsum+=currentmax;
-                currentmax=0;
-                mini=prices[i];
+               return dp[idx][flag]=max(prices[idx]+sellstock(idx+1,prices,1,dp),sellstock(idx+1,prices,0,dp));
             }
         }
-        return totalsum+currentmax;
-        
+    int maxProfit(vector<int> &prices)
+    {
+        int idx = 0;
+        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+        return sellstock(idx, prices, 1,dp);
     }
 };
