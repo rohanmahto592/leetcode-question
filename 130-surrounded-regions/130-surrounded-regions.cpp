2cfa4fb992e4a4885e13aafc4f125b0d60ccb972
Rohan Mahto <68339841+rohanmahto592@users.noreply.github.com>
2022-09-28 03:37:24 +0530

    Time: 6 ms (99.70%), Space: 9.8 MB (92.07%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,64 +1,63 @@
-class Solution {
-public:
-    void flip(int i, int j,vector<vector<char>>& board)
+class Solution
+{
+    public:
+        void flip(int i, int j, vector<vector < char>> &board)
+        {
+            if (i < 0 or j < 0 or i >= board.size() or j >= board[0].size() or board[i][j] == 'X'
+                or board[i][j] == '1')
+            {
+                return;
+            }
+
+            board[i][j] = '1';
+            flip(i - 1, j, board);
+            flip(i, j + 1, board);
+            flip(i, j - 1, board);
+            flip(i + 1, j, board);
+        }
+    void solve(vector<vector < char>> &board)
     {
-        if(i<0 or j<0 or i>=board.size() or j>=board[0].size() or board[i][j]=='X' or board[i][j]=='1')
+        for (int j = 0; j < board[0].size(); j++)
         {
-            return ;
-        }
-       
-        board[i][j]='1';
-        flip(i-1,j,board);
-        flip(i,j+1,board);
-        flip(i,j-1,board);
-        flip(i+1,j,board);
-    }
-    void solve(vector<vector<char>>& board) {
-        for(int j=0;j<board[0].size();j++)
-        {
-            if(board[0][j]=='O')
+            if (board[0][j] == 'O')
             {
-                flip(0,j,board);
+                flip(0, j, board);
             }
         }
-         for(int i=0;i<board.size();i++)
+        for (int i = 0; i < board.size(); i++)
         {
-            if(board[i][0]=='O')
+            if (board[i][0] == 'O')
             {
-                flip(i,0,board);
+                flip(i, 0, board);
             }
         }
-         for(int i=0;i<board.size();i++)
+        for (int i = 0; i < board.size(); i++)
         {
-            if(board[i][board[0].size()-1]=='O')
+            if (board[i][board[0].size() - 1] == 'O')
             {
-                flip(i,board[0].size()-1,board);
+                flip(i, board[0].size() - 1, board);
             }
         }
-         for(int j=0;j<board[0].size();j++)
+        for (int j = 0; j < board[0].size(); j++)
         {
-            if(board[board.size()-1][j]=='O')
+            if (board[board.size() - 1][j] == 'O')
             {
-                flip(board.size()-1,j,board);
+                flip(board.size() - 1, j, board);
             }
         }
-        for(int i=0;i<board.size();i++)
+        for (int i = 0; i < board.size(); i++)
         {
-            for(int j=0;j<board[0].size();j++)
+            for (int j = 0; j < board[0].size(); j++)
             {
-                if(board[i][j]=='1')
+                if (board[i][j] == '1')
                 {
-                    board[i][j]='O';
+                    board[i][j] = 'O';
                 }
-                else if(board[i][j]=='O')
+                else if (board[i][j] == 'O')
                 {
-                    board[i][j]='X';
+                    board[i][j] = 'X';
                 }
             }
         }
-        
-        
-        
-        
     }
 };
